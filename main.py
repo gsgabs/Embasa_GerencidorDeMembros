f45c1e59d23634948e6dc05e1799aecf7497e8d2
Gabriel <135984152+gsgabs@users.noreply.github.com>
2025-03-02 10:30:58 -0300

    Criação main.py, e banco Cadastro

---
+++ main.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+try:
+    conn = sqlite3.connect("CadastroDB.db")
+    print("Conexão com o banco de dados realizada com sucesso.")
+except sqlite3.Error as erro:
+    print("Houve um erro: ", erro)
+
+
+
+
