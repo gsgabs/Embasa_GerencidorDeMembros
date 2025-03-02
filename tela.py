b44fbcf232439cfa386b1a292b7f7ae8fa41883b
Gabriel <135984152+gsgabs@users.noreply.github.com>
2025-03-02 10:54:24 -0300

    Adição das Cores

--- tela.py
+++ tela.py
@@ -1,2 +1,25 @@
 from tkinter import *
 from tkinter import ttk
+
+cor1 = "E6E6FA"  #Lavanda
+cor2 = "EE82EE"  #Violeta
+cor3 = "4B0082"  #Indigo
+cor4 = "483D8B"  #DarkSlateBlue
+cor5 = "DC143C"  #Carmesim
+cor6 = "FF0000"  #Vermelho
+cor7 = "B22222"  #Firebrick
+cor8 = "8B0000"  #Vemelho Escuro
+cor9 = "FFC0CB"  #Pink
+cor10 = "FFA500" #Orange
+cor11 = "FFD700" #Gold
+cor12 = "FFDAB9" #Pêssego
+cor13 = "ADFF2F" #Verde Amarelo
+cor14 = "00FF00" #Limão
+cor15 = "008000" #Verde
+cor16 = "66CDAA" #Água Marinha
+cor17 = "00FFFF" #Água
+cor18 = "E0FFFF" #Light Cyan
+cor19 = "40E0D0" #Turquesa
+cor20 = "00BFFF" #DeepSkyBlue
+
+
