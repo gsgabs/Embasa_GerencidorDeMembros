f8e769039f0c39cd6aa498438b03e3e7217f7e43
Gabriel <135984152+gsgabs@users.noreply.github.com>
2025-03-03 15:47:36 -0300

    Criando Tabela do banco

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import sqlite3
 
+#Inicializando Banco----------------------------------------------------------
 try:
     conn = sqlite3.connect("CadastroDB.db")
     print("Conexão com o banco de dados realizada com sucesso.")
 except sqlite3.Error as erro:
-    print("Houve um erro: ", erro)
+    print("Houve um erro ao se conectar com o banco de dados: ", erro)
+    exit()
+
+#Criando Cursor
+cursor = conn.cursor()
+
+#Apagando tabela para testar
+cursor.execute("DROP TABLE IF EXISTS Membros")
+
+#Criando Tabela
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Membros (
+    prontuario INTEGER PRIMARY KEY,
+    nome TEXT NOT NULL,
+    telefone TEXT,
+    data_nascimento DATE NOT NULL,
+    data_inscricao DATE NOT NULL
+    data_vencimento DATE NOT NULL
+    )
+""")
+
 
 
 
