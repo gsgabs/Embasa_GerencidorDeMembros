b37ffecb328301bd5283b2aa2269bed78d5770f2
Gabriel <135984152+gsgabs@users.noreply.github.com>
2025-03-03 16:28:01 -0300

    Testes com o banco

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ cursor.execute("""
     nome TEXT NOT NULL,
     telefone TEXT,
     data_nascimento DATE NOT NULL,
-    data_inscricao DATE NOT NULL
+    data_inscricao DATE NOT NULL,
     data_vencimento DATE NOT NULL
     )
 """)
 
 tempo_de_vencimento = '+90 days'
 
-#CRUD Create Read Update Delete
+#CRUD Create Read Update Delete-----------------------------------------------
 
 #Inserir Membro C
 def inserir_membro(values):
@@ -52,8 +52,8 @@ def read_membro():
 
 #Atualizando Membros U
 def update_membro(values):
-    query = """"
-        UPDATE Membros Set nome = ?, telefone = ?, data_nascimento = ?, data_inscrição = ?, data_vencimento = ? WHERE prontuario = ?
+    query = """
+        UPDATE Membros Set nome = ?, telefone = ?, data_nascimento = ?, data_inscricao = ?, data_vencimento = ? WHERE prontuario = ?
     """
     cursor.execute(query, values)
 
@@ -62,15 +62,37 @@ def delete_membro(valor):
     query = "DELETE FROM Membros WHERE prontuario = ?"
     cursor.execute(query, (valor,))
 
+#Test Area ------------------------------------------------
+
+# Inserindo diretamente para teste
+cursor.execute("""
+    INSERT INTO Membros (nome, telefone, data_nascimento, data_inscricao, data_vencimento)
+    VALUES
+    ('Joana D Arc', '11987654321', '1412-01-06', DATE('now'), DATE('now', ?)),
+    ('Cristovão Colombo', '21987654321', '1451-10-31', DATE('now'), DATE('now', ?)),
+    ('Ezio Auditore', '31987654321', '1459-06-24', DATE('now'), DATE('now', ?))
+""", (tempo_de_vencimento, tempo_de_vencimento, tempo_de_vencimento))
 
 
+# Exemplo de uso da função inserir_membro
+valores_inserir = ['Leonardo da Vinci', '40028922', '1401-01-01']
+inserir_membro(valores_inserir)
 
+valores_atualizar = ('Abraham Lincoln', '3321156', '1777-02-04', '2025-01-01', '2025-06-01', 4)
+update_membro(valores_atualizar)
 
+delete_membro(3)
 
+for item in read_membro():
+    print(item)
 
+#Finalizando --------------------------------------------------
 
+# Salvando as alterações
+conn.commit()
 
-
+# Fechando a conexão
+conn.close()
 
 
 
